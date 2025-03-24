10ca8904a63acff818b208a3b476bf60e067c04c
Roman <bajcukroma@gmail.com>
2025-03-24 11:06:58 +0600

    Отредактированы тесты. внутри них вызываются только высокоуровневые команды.

--- test_add_group.py
+++ test_add_group.py
@@ -18,6 +18,7 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element(By.LINK_TEXT, "group page").click()
 
     def create_group(self, wd, group):
+        self.open_groups_page(wd)
         # fill group form
         wd.find_element(By.NAME, "new").click()
         wd.find_element(By.NAME, "group_name").click()
@@ -31,11 +32,13 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element(By.NAME, "group_footer").send_keys(group.footer)
         # submit group creation
         wd.find_element(By.NAME, "submit").click()
+        self.return_to_groups_page(wd)
 
     def open_groups_page(self, wd):
         wd.find_element(By.LINK_TEXT, "groups").click()
 
     def login(self, wd, username, password):
+        self.open_home_page(wd)
         wd.find_element(By.NAME, "user").click()
         wd.find_element(By.NAME, "user").clear()
         wd.find_element(By.NAME, "user").send_keys(username)
@@ -49,20 +52,14 @@ class TestAddGroup(unittest.TestCase):
 
     def test_add_group(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd, "admin", "secret")
-        self.open_groups_page(wd)
         self.create_group(wd, Group("asd", "asd", "asd"))
-        self.return_to_groups_page(wd)
         self.logout(wd)
 
     def test_add_empty_group(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd, "admin", "secret")
-        self.open_groups_page(wd)
         self.create_group(wd, Group("", "", ""))
-        self.return_to_groups_page(wd)
         self.logout(wd)
 
     def is_element_present(self, how, what):
